7010d1e33c35fa855213b61cc53fe55f6b6ab5b3
Haley Seymour <haleycseymour@comcast.net>
2022-09-04 11:51:02 -0700

    confirmed all routes working well

--- controllers/api/songRoutes.js
+++ controllers/api/songRoutes.js
@@ -1,96 +1,90 @@
 const express = require("express");
 const router = express.Router();
-const {User,Song} = require("../../models");
+const { User, Song } = require("../../models");
 
-// good
-router.get("/", async (req,res) => {
-    try {
-        const songData = await Song.findAll({
-            include:[User]
-        })
-        
-        res.status(200).json(songData);
-    } catch (err) {
-       console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
+router.get("/", async (req, res) => {
+  try {
+    const songData = await Song.findAll({
+      include: [User],
+    });
+    res.status(200).json(songData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const songData = await Song.findByPk(req.params.id, {
+      include: [User],
+    });
+    if (!songData) {
+      res.status(404).json({ msg: "song does not exist" });
+    } else {
+      res.json(songData);
     }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
 
-// 
-router.get("/:id", async (req,res) => {
-    try {
-        const songData = await Song.findByPk(req.params.id, {
-            include:[User]
-        })
-        if (!songData){
-            res.status(404).json({msg: "song does not exist"})
-        } else {
-        res.json(songData)
-        }
-        
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
-    }
-})
-
-// good
 router.post("/", async (req, res) => {
-    try {
-        const songData = await Song.create(
-            {
-                song_name: req.body.song_name,
-                artist: req.body.artist,
-                genre: req.body.genre
-              },
-        );
-        res.status(200).json(songData);
+  try {
+    const songData = await Song.create({
+      song_name: req.body.song_name,
+      artist: req.body.artist,
+      genre: req.body.genre,
+    });
+    res.status(200).json(songData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
 
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
+router.put("/:id", async (req, res) => {
+  try {
+    const response = await Song.update(
+      {
+        song_name: req.body.song_name,
+        artist: req.body.artist,
+        genre: req.body.genre,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!response[0]) {
+      res.status(404).json({ msg: "song does not exist" });
+    } else {
+      res.json({ msg: "successfully updated song" });
     }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
 
-router.put("/:id", async (req,res) => {
-    try {
-        const response = await Song.update(
-            {
-                song_name: req.body.song_name,
-                artist: req.body.artist,
-                genre: req.body.genre
-              },
-              {
-                where: {
-                    id: req.params.id
-                }
-            }
-        )
-        if (!response[0]){
-            res.status(404).json({msg: "song does not exist"})
-        } else {
-            res.json("successfully updated song");
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
+router.delete("/:id", async (req, res) => {
+  try {
+    const response = await Song.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (response == 0) {
+      res.status(404).json({ msg: "song does not exist" });
+    } else {
+      res.json("song removed from database");
     }
-})
-
-router.delete("/:id", async (req,res) => {
-    try {
-        const response = Song.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        console.log(response)
-        // does not show if song was existant in database but that is okay - either way not in database
-        res.json("song removed from database")
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
-    }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,87 +2,86 @@ const express = require("express");
 const router = express.Router();
 const { User, Song } = require("../../models");
 
-router.get("/", async (req,res) => {
-    try {
-        const userData = await User.findAll({
-            include:[Song]
-        })
-        console.log(userData)
-        res.status(200).json(userData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err: err });
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      include: [Song],
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err: err });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      include: [Song],
+    });
+    if (!userData) {
+      res.status(404).json({ msg: "user does not exist" });
+    } else {
+      res.json(userData);
     }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err: err });
+  }
+});
 
-router.get("/:id", async (req,res) => {
-    try {
-        const userData = await User.findByPk(req.params.id, {
-            include: [Song]
-        })
-        // if (!userData){
-        //     res.status(404).json({msg: "user does not exist"})
-        // } 
-        res.json(userData)
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err: err });
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      name: req.body.name,
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const response = await User.update(
+      {
+        name: req.body.name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!response[0]) {
+      res.status(404).json({ msg: "user does not exist" });
+    } else {
+      res.json({ msg: "successfully updated user" });
     }
-})
-
-router.post("/", async (req,res) => {
-    try {
-        const userData = await User.create(
-            {
-            name: req.body.name,
-            });
-        res.status(200).json(userData);
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
-    }
-})
-
-router.put("/:id", async (req,res) => {
-    try {
-        const updatedUser = await User.update(
-            {
-                name: req.body.name,
-            }, {
-                where: {
-                    id: req.params.id
-                }
-            }
-        )
-        // if (!updatedUser[0]){
-        //     res.status(404).json({msg: "user does not exist"})
-        // }
-        res.json(updatedUser);
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "an error occured", err });
-    }
-})
-
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  }
+});
 
 // need to debug this route - successfully deleting but also catching
-router.delete("/:id", async (req,res) => {
-    try {
-        const delUser = await User.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        // if (!delUser[0]){
-        //     res.status(404).json({msg: "user does not exist"})
-        // }
-        res.json(delUser)
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "2", err: err});
+router.delete("/:id", async (req, res) => {
+  try {
+    const response = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (response == 0) {
+      res.status(404).json({ msg: "user does not exist" });
+    } else {
+      res.json({ msg: "user removed from database" });
     }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err: err });
+  }
+});
 
 module.exports = router;
