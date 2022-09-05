2c4316ff39cdbc919c41b0f65a14d5917fbffd78
Haley Seymour <haleycseymour@comcast.net>
2022-09-05 12:57:03 -0700

    js and partial change

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,22 @@
 const addBtn = document.querySelector("#addFormSubmit");
-const songName = document.querySelector('#songName').value;
-const artist = document.querySelector('#artist').value;
-const genre = document.querySelector('#genre').value;
-const user = document.querySelector('#user').value;
+const songName = document.querySelector('#songName');
+const artist = document.querySelector('#artist');
+const genre = document.querySelector('#genre');
+const userName = document.querySelector('#user');
 
 addBtn.addEventListener("click", function(e){
     e.preventDefault();
+    // fetch post user - if success, use user id
+    // if not successful, read all user and match for id
+    const user = userName.value;
+    
     const songObj = {
-        song_name:songName,
-        artist:artist,
-        genre:genre
-        // 
+        song_name:songName.value,
+        artist:artist.value,
+        genre:genre.value,
+        // UserId:
     }
-    console.log('clicked')
+    console.log(songObj)
 })
+
+// e.target.dataset.id for grabbing delete and update id
